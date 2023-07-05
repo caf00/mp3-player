6b89610c4f398de81705b658533ad0a634103417
alejandro <cesar.alejandro.fernandez.7@gmail.com>
2023-07-04 23:10:52 -0300

    added method that creates the music folder if it doesn't exist

--- src/main/java/alexdev/mp3/Mp3Application.java
+++ src/main/java/alexdev/mp3/Mp3Application.java
@@ -1,13 +1,37 @@
 package alexdev.mp3;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
 
 @SpringBootApplication
 public class Mp3Application {
 
+	private static final Logger log = LoggerFactory.getLogger(Mp3Application.class);
+
+	@Value("${music.directory}")
+	private String MUSIC_DIRECTORY;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Mp3Application.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		File musicFolder = new File(MUSIC_DIRECTORY);
+		if (!musicFolder.exists()) {
+			boolean created = musicFolder.mkdirs();
+			if (created) {
+				log.info("Carpeta 'music' creada en " + MUSIC_DIRECTORY);
+			} else {
+				log.error("No se pudo crear la carpeta 'music' en " + MUSIC_DIRECTORY);
+			}
+		}
 	}
 
 }
